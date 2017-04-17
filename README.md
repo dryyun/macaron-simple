8574271c1c71d6843c17e0d1a6add01ff291202c
dryyun <dryyun@gmail.com>
2017-04-17 19:17:58 +0800

    commit

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 作为 go 小白，尝试使用 macaron 构建 curd 应用   
 学习使用 go，go-web 知识
 
-使用第三方库
-> macaron  - https://github.com/go-macaron/macaron - Go Web 框架   
+工具
 > govendor - https://github.com/kardianos/govendor - Go 包管理工具  
 > realize  - https://github.com/tockins/realize    - Go 工具，实现文件监控，热编辑等  
+> goose    - https://github.com/pressly/goose      - 数据结构迁移工具
+
+使用第三方库   
+> macaron  - https://github.com/go-macaron/macaron - Go Web 框架   
 
 
 
---
+++ database/20170415093132_AddUserTable.sql
@@ -0,0 +1,19 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255)  NOT NULL,
+  `phone` varchar(11)  NOT NULL,
+  `password` varchar(128)  NOT NULL,
+  `avatar` varchar(256) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '0',
+  `created` int(11) DEFAULT '0',
+  `updated` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS `user`  ;
